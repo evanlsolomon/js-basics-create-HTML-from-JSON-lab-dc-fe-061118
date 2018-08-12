24b030dd18df2b910afa8ddfa72cac5f9d136c3c
Evan Solomon <evanlsolomon@gmail.com>
2018-08-12 19:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function listCast(cast){
 function listReviews(reviews){
   for(var review of reviews){
   let newReview = document.createElement("li");
-  newReview.innerHTML = person.role + ": " + person.actor;
+  newReview.innerHTML = review.person + ": " + review.content;
   document.getElementById('cast').appendChild(newCastMember)
   }
 }
